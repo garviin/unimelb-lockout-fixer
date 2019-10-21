17b28bb14a10d0245edc7b43cb7bdf873a73458e
garviin <garvibulkin@gmail.com>
2019-10-21 18:51:11 +1100

    implemented ISE login script

--- fixer.py
+++ fixer.py
@@ -1,4 +1,9 @@
 import subprocess
+import json
+from getpass import getpass
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+import time
 
 def __auth_cisco_vpn__(username, pin, domain):
     # Assign cmd
@@ -15,3 +20,22 @@ def __auth_cisco_vpn__(username, pin, domain):
                      executable="/bin/bash",
                      stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE).communicate()
+
+def ise_login(username, password, link):
+    opts = Options()
+    opts.headless = False
+    browser = Firefox(options=opts)
+    browser.get(link)
+    time.sleep(2)
+
+    username = browser.find_element_by_id("dijit_form_TextBox_0")
+    password = browser.find_element_by_id("dijit_form_TextBox_1")
+    submit   = browser.find_element_by_id("loginPage_loginSubmit")
+
+    username.send_keys(USERNAME)
+    password.send_keys(PASSWORD)
+    submit.click()
+
+    time.sleep(13)
+
+    browser.find_element_by_id("carousel-next").click()
